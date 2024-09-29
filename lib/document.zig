5584136d6f35538f31e0979c38d900a2e22fdbf5
Marlon Rosenberg <marlon.rosenberg@t-online.de>
2024-09-30 01:50:08 +0200

    feat(document): add setFromFilePath

--- lib/document.zig
+++ lib/document.zig
@@ -4,6 +4,8 @@ const parser = @import("parser.zig");
 const Element = @import("elements.zig").Element;
 const Self = @This();
 
+const max_append_size = 10_000;
+
 allocator: std.mem.Allocator,
 content: std.ArrayList(u8),
 elements: std.ArrayList(Element),
@@ -25,7 +27,22 @@ pub fn setContent(self: *Self, content: []const u8) !void {
     try self.content.appendSlice(content);
 
     self.elements.clearAndFree();
-    try parser.parse(content, &self.elements);
+    try parser.parse(self.content.items, &self.elements);
+}
+
+/// Sets the content of the document from a file and parses it into elements.
+/// Clears any existing content and elements.
+/// Accessing the document old elements after calling this function is undefined behavior.
+pub fn setContentFromFile(
+    self: *Self,
+    /// The relative path to the file.
+    rel_path: []const u8,
+) !void {
+    self.content.clearAndFree();
+    var file = try std.fs.cwd().openFile(rel_path, .{});
+    try file.reader().readAllArrayList(&self.content, max_append_size);
+    self.elements.clearAndFree();
+    try parser.parse(self.content.items, &self.elements);
 }
 
 /// Deinitializes the document, freeing all resources.
@@ -68,4 +85,28 @@ test "Full SetContent" {
     try testing.expectEqual(1, elements[2].list.items().len);
 
     try testing.expectEqualSlices(u8, "Paragraph text", elements[3].paragraph.text);
+}
+
+test "from file" {
+    const allocator = std.testing.allocator;
+    var document = Self.init(allocator);
+    defer document.deinit();
+    var tmp_dir = testing.tmpDir(.{});
+    defer tmp_dir.cleanup();
+
+    var file = try tmp_dir.dir.createFile("test_document.md", .{});
+    var writer = file.writer();
+    try writer.writeAll("# Heading\n");
+
+    const path = try std.fs.path.join(
+        allocator,
+        &[_][]const u8{ ".zig-cache", "tmp", &tmp_dir.sub_path, "test_document.md" },
+    );
+    defer allocator.free(path);
+
+    try document.setContentFromFile(path);
+    const elements = document.elements.items;
+    try testing.expectEqualSlices(u8, "# Heading\n", document.content.items);
+    try testing.expectEqualSlices(u8, "Heading", elements[0].heading.text);
+    try testing.expectEqual(1, elements[0].heading.level);
 }
--- lib/root.zig
+++ lib/root.zig
@@ -1,6 +1,6 @@
 pub const parser = @import("parser.zig");
 pub const elements = @import("elements.zig");
-pub const document = @import("document.zig");
+pub const Document = @import("document.zig");
 
 test {
     @import("std").testing.refAllDecls(@This());
